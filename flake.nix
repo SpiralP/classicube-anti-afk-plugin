175ec8ccf84919aea4cda8d148f31382aba1abab
SpiralP <spiral.palmer@gmail.com>
2024-07-03 19:31:17 -0700

    update deps

--- flake.nix
+++ flake.nix
@@ -1,68 +1,74 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
-    in
-    builtins.foldl' lib.recursiveUpdate { } (builtins.map
-      (system:
+
+      makePackages = (system: dev:
         let
           pkgs = import nixpkgs {
             inherit system;
           };
+          rustManifest = lib.importTOML ./Cargo.toml;
 
-          inherit (pkgs) rustPlatform;
-          package = rustPlatform.buildRustPackage {
-            name = "classicube-anti-afk-plugin";
-            src = lib.cleanSourceWith rec {
-              src = ./.;
-              filter = path: type:
-                lib.cleanSourceFilter path type
-                && (
-                  let
-                    baseName = builtins.baseNameOf (builtins.toString path);
-                    relPath = lib.removePrefix (builtins.toString ./.) (builtins.toString path);
-                  in
-                  lib.any (re: builtins.match re relPath != null) [
-                    "/Cargo.toml"
-                    "/Cargo.lock"
-                    "/src"
-                    "/src/.*"
-                  ]
-                );
+          defaultAttrs = {
+            pname = rustManifest.package.name;
+            version = rustManifest.package.version;
+
+            src = lib.sourceByRegex ./. [
+              "^\.cargo(/.*)?$"
+              "^build\.rs$"
+              "^Cargo\.(lock|toml)$"
+              "^src(/.*)?$"
+            ];
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "async-dispatcher-0.1.0" = "sha256-rqpQ176/PnI9vvPrwQvK3GJbryjb3hHkb+o1RyCZ3Vg=";
+                "classicube-helpers-2.0.0+classicube.1.3.6" = "sha256-V5PBZR0rj42crA1fGUjMk4rDh0ZpjjNcbMCe6bgotW8=";
+              };
             };
-            cargoSha256 = "sha256-rcs8DUvxiHBamOh5h5IwH7VYtzWUOeY0ubArsVGyJpg=";
+
             nativeBuildInputs = with pkgs; [
               pkg-config
               rustPlatform.bindgenHook
-            ];
-            buildInputs = with pkgs; [
-              alsa-lib
-              at-spi2-atk
-              cairo
-              gdk-pixbuf
-              glib
-              gtk3
-              openssl
-              pango
-            ];
-
-            doCheck = false;
+            ] ++ (if dev then
+              with pkgs; ([
+                cargo-release
+                clippy
+                rustfmt
+                rust-analyzer
+              ]) else [ ]);
           };
         in
-        rec {
-          devShells.${system}.default = package.overrideAttrs (old: {
-            nativeBuildInputs = with pkgs; old.nativeBuildInputs ++ [
-              clippy
-              rustfmt
-              rust-analyzer
-            ];
-          });
-          packages.${system}.default = package;
+        {
+          default = pkgs.rustPlatform.buildRustPackage defaultAttrs;
+
+          debug = (pkgs.enableDebugging {
+            inherit (pkgs) stdenv;
+            override = (attrs: pkgs.makeRustPlatform ({
+              inherit (pkgs) rustc cargo;
+            } // attrs));
+          }).buildRustPackage (
+            (defaultAttrs // {
+              pname = "${defaultAttrs.pname}-debug";
+
+              buildType = "debug";
+
+              hardeningDisable = [ "all" ];
+            })
+          );
         }
-      )
+      );
+    in
+    builtins.foldl' lib.recursiveUpdate { } (builtins.map
+      (system: {
+        devShells.${system} = makePackages system true;
+        packages.${system} = makePackages system false;
+      })
       lib.systems.flakeExposed);
 }
