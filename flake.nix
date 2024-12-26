a43b62f523fe9916e2fb98e95b76f897a6bcb7a2
SpiralP <spiral.palmer@gmail.com>
2024-12-26 14:14:47 -0800

    nix: use commit hash in version; use allowBuiltinFetchGit so we don't have to manage hashes

--- flake.nix
+++ flake.nix
@@ -3,20 +3,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs }:
     let
       inherit (nixpkgs) lib;
+
+      rustManifest = lib.importTOML ./Cargo.toml;
 
       makePackages = (system: dev:
         let
           pkgs = import nixpkgs {
             inherit system;
           };
-          rustManifest = lib.importTOML ./Cargo.toml;
 
           defaultAttrs = {
             pname = rustManifest.package.name;
-            version = rustManifest.package.version;
+            version = "${rustManifest.package.version}-${self.shortRev or self.dirtyShortRev}";
 
             src = lib.sourceByRegex ./. [
               "^\.cargo(/.*)?$"
@@ -27,10 +28,7 @@
 
             cargoLock = {
               lockFile = ./Cargo.lock;
-              outputHashes = {
-                "async-dispatcher-0.1.0" = "sha256-rqpQ176/PnI9vvPrwQvK3GJbryjb3hHkb+o1RyCZ3Vg=";
-                "classicube-helpers-3.0.0+classicube.1.3.7" = "sha256-3hWKS6NmAH0x+SOi/nBKJLIQi/3ilG7WSRrPvF++wGE=";
-              };
+              allowBuiltinFetchGit = true;
             };
 
             nativeBuildInputs = with pkgs; [
